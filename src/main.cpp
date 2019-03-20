04e10081492d1c925e66ebcdd93023b8bd9aac76
Student <student@ib.polsl.pl>
2019-03-20 14:05:42 +0100

    Czwarty commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,28 @@
+#include "LaborkaConfig.h"
+#ifdef USE_TRIGONOMETRY_DEGREE
 #include "trygonometria.h"
+#else
+#include <cmath>
+#endif
+
 #include <iostream>
 
 int main() 
 { 
-	double stopnie=90;
-	double sin = degreemath::sin(stopnie);
-	double cos = degreemath::cos(stopnie);
-	double tg = degreemath::tg(stopnie);
-	double ctg = degreemath::ctg(stopnie);
+	double kat=90;
+
+	#ifdef USE_TRIGONOMETRY_DEGREE
+	double sin = degreemath::sin(kat);
+	double cos = degreemath::cos(kat);
+	double tg = degreemath::tg(kat);
+	double ctg = degreemath::ctg(kat);
+	#else
+	double sin = std::sin(kat);
+	double cos = std::cos(kat);
+	double tg = std::tan(kat);
+	double ctg = 1/std::tan(kat);
+	#endif
+
 
 	std::cout << "Sinus = ";
 	std::cout << sin << std::endl;
